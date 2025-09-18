69981d74b73f55398ff9fadf08d95b3a2d6c36fb
Alexey Poimtsev <alexey.poimtsev@gmail.com>
2025-09-18 22:38:07 +0300

    fix: add missing initializer for better_html gem

--- src/shared/gems_better_html.rb
+++ src/shared/gems_better_html.rb
@@ -1,2 +1,9 @@
 copy_file "spec/better_html_spec.rb", force: true
 copy_file "app/helpers/application_helper.rb", force: true
+
+initializer "better_html.rb", <<-CODE
+  BetterHtml.configure do |config|
+    config.template_exclusion_filter = proc { |filename| !filename.start_with?(Rails.root.to_s) }
+    config.partial_tag_name_pattern = /\\A[a-zA-Z0-9\\-:]+\\z/
+  end
+CODE
