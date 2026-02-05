20fea52ba67769e02373ba7233e182c1f2fa25d5
Alexey Poimtsev <alexey.poimtsev@gmail.com>
2026-02-05 17:23:26 +0300

    chore: improve generators in inertia_shared

--- src/inertia_shared/init_generators.rb
+++ src/inertia_shared/init_generators.rb
@@ -1,13 +1,11 @@
 # Configure generators for Inertia - skip JavaScript, assets, and helpers
-application do
-  <<-RUBY
-    config.generators do |g|
+initializer "generators.rb", <<-CODE
+  Rails.application.config.generators do |g|
       g.orm :active_record, primary_key_type: :uuid
       g.test_framework :rspec, view_specs: false
       g.assets false
       g.helper false
       g.javascripts false
       g.stylesheets false
-    end
-  RUBY
-end
+  end
+CODE
